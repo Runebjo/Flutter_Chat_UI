214d44849834fb009991abcb97861faa4202a902
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-10-09 22:08:07 +0200

    ADD category_selector

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/widgets/category_selector.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,7 +10,35 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          CategorySelector(),
+        ],
+      ),
     );
   }
 }
